fee471b0be2bfa2a76ac898279135fe684120742
pawan kumar <98738334+thepawankumar@users.noreply.github.com>
2022-11-22 17:06:01 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,17 +1,5 @@
-terraform {
-  required_providers {
-    azurerm = {
-        source  = "hashicorp/azurerm"
-        version = "=3.0.0"
-    }
-  }
-  backend "azurerm" {
-    resource_group_name     = "rg-terraform-state-001"
-    storage_account_name    = "cloudninjaterraformstate"
-    container_name          = "tfstate"
-    key                     = "GitHub-Terraform-rg-loganalytics-001"
-  }
-}
-provider "azurerm" {
-  features {}
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
